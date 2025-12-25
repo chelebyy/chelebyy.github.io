47a545b090ab17cc560aa973092febabeabc60d7
chelebyy <muhammett.celebi@gmail.com>
2025-12-26 02:17:14 +0300

    fix: set base path for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // Important for GitHub Pages to find assets
   server: {
     port: 3000,
     host: '0.0.0.0',
